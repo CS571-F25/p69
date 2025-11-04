1fb1c0a455fc32cf150a49cd1a61c10648a78507
Calvin DeBellis <calvindebellis@Calvins-MacBook-Pro.local>
2025-11-04 15:57:45 -0600

    new build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
-base: '/571-final-project/',
+base: '/p69/',
 build: {
 outDir: 'docs'
 }
